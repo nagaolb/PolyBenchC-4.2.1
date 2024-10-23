6d8a28cea7d1ac2b9f7085a5c8373dc51bd35966
nagaolb <nagaolb@pear.is.utsunomiya-u.ac.jp>
2024-10-23 18:55:50 +0000

    Use O2 for Release build

--- cmake.sh
+++ cmake.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p build/gcc && cmake -S. -Bbuild/gcc -DCMAKE_TOOLCHAIN_FILE=cmake/aarch64-linux-gnu.toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 mkdir -p build/clang && cmake -S. -Bbuild/clang -DCMAKE_TOOLCHAIN_FILE=cmake/aarch64-clang.toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
+mkdir -p build/gcc-o2 && cmake -S. -Bbuild/gcc-o2 -DCMAKE_TOOLCHAIN_FILE=cmake/aarch64-linux-gnu.toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+mkdir -p build/clang-o2 && cmake -S. -Bbuild/clang-o2 -DCMAKE_TOOLCHAIN_FILE=cmake/aarch64-clang.toolchain.cmake -DCMAKE_BUILD_TYPE=Release
