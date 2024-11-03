18b6f8001afca2a58ab52e41f077ab72f2836a62
nagaolb <nagaolb@pear.is.utsunomiya-u.ac.jp>
2024-11-03 10:29:21 +0000

    Do the O2 build as well

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ BUILD_DIR=build
 mkdir -p ${BUILD_DIR}
 bash cmake.sh
 pushd ${BUILD_DIR}/gcc && make -j && popd
+pushd ${BUILD_DIR}/gcc-o2 && make -j && popd
 pushd ${BUILD_DIR}/clang && make -j && popd
+pushd ${BUILD_DIR}/clang-o2 && make -j && popd
 bash copy_bin.sh ${BUILD_DIR} ${BUILD_DIR}/bin
 bash disass_dir.sh ${BUILD_DIR}/bin ${BUILD_DIR}/asm
